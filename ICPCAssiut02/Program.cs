f24f7b6b79d21d6e291ad94866f825d155ed0212
entsarahmed <entesara500@gmail.com>
2025-02-15 19:28:02 +0200

    A. 1 to N

--- ICPCAssiut02/Program.cs
+++ ICPCAssiut02/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region A. 1 to N
+
+        /*    int N;
+            bool Flag;
+
+            do
+            {
+                Console.WriteLine("Enter Your N");
+                Flag = int.TryParse(Console.ReadLine(), out N);
+            } while (!Flag);
+            for (int i = 1; i<=N; i++)
+                Console.WriteLine(i); */
+
+            #endregion
+
+
         }
     }
 }
