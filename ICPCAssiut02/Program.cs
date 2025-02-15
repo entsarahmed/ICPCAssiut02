7428eb6cd254cfc0a9bd9daa3ce98c5c75d7b98d
entsarahmed <entesara500@gmail.com>
2025-02-15 19:23:02 +0200

    Add project files.

---
+++ ICPCAssiut02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ICPCAssiut02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
